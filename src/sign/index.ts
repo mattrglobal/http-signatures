273a444dc58b7a3c9fb9a9886073eec49d01339b
James Collins <james.collins@mattr.global>
2020-11-30 16:08:21 +1300

    feat: sign

--- src/sign/index.ts
+++ src/sign/index.ts
@@ -18,25 +18,56 @@ const generateObjectEntriesString = (entries: [string, string | number][]): stri
     })
     .join("\n");
 
-// see https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12#section-2.3
+/**
+ * Generate a string representation of an object and return the bytes of that string for signing
+ * @see https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12#section-2.3
+ */
 const generateBytesToSign = pipe(lowerCaseObjectKeys, Object.entries, generateObjectEntriesString, stringToBytes);
-// Generates a string containing all the keys of an object
+
+/**
+ * Generate a string containing all the keys of an object separated by a space
+ * @see https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12#section-2.1.6
+ */
 const generateHeadersListString = pipe(lowerCaseObjectKeys, Object.keys, joinWithSpace);
 
 export type CreateSignatureHeaderOptions = {
   readonly signer: {
+    /**
+     * The key id used for creating the signature. This will be added to the signature string and used in verification
+     */
     readonly keyId: string;
+    /**
+     * The function for signing the data with the hs2019 algorithm
+     */
     readonly sign: (data: Uint8Array) => Promise<Uint8Array>;
   };
+  /**
+   * Full url of the request including query parameters
+   */
   readonly url: string;
+  /**
+   * The HTTP request method of the request
+   */
   readonly method: string;
+  /**
+   * Headers and their values to include in the signing
+   * The keys of these headers will be appended to the signature string for verification
+   */
   readonly headers: { readonly [key: string]: string };
+  /**
+   * The body of the request
+   */
   readonly body?: object | string;
 };
 
+/**
+ * Creates a signature header to be appended as a header on a request
+ * A digest header will be returned if a body was included. This also needs to be appended to the request headers.
+ * @see https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12#section-4
+ */
 export const createSignatureHeader = async (
   options: CreateSignatureHeaderOptions
-): Promise<{ digest: string; signature: string }> => {
+): Promise<{ digest?: string; signature: string }> => {
   const algorithm = "hs2019";
   const {
     signer: { keyId, sign },
@@ -68,7 +99,7 @@ export const createSignatureHeader = async (
   )}"`;
 
   return {
-    digest: dataToSign.digest,
     signature: signatureHeaderValue,
+    ...(dataToSign.digest ? { digest: dataToSign.digest } : {}),
   };
 };
